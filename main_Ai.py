7ee8cbae8a2dee3343eefa1857b96f39d464ef12
pheechan <6640208922@student.chula.ac.th>
2023-12-09 01:22:29 +0700

    commit 11

--- main_Ai.py
+++ main_Ai.py
@@ -2,16 +2,7 @@ import streamlit as st
 import openai
 import pandas as pd
 import json
-# from langchain.chat_models import GPTAI
-# from langchain.schema import (
-#     SystemMessage,
-#     HumanMessage,
-#     AIMessage
-# )
 
-# from streamlit_chat import Message
-
-# pip install streamlit-chat
 prompt1 = """Act as an AI writing analizer in English. You will receive a 
             piece of generated writing from Ai and you will rewrite and improve the writing and make the writing more human-like making it less-detectable that it was from ai.
             Say only the writing that you generated, don't say anything else.
@@ -20,7 +11,7 @@ prompt2 = """Act as an AI writing analizer in German, French, Spanish. You will 
             Then you must find interesting vocabulary and store them in a list.
 
         """
-#pip install langchain openai
+
 
 def init():
     # Set up the streamlit app
@@ -29,65 +20,37 @@ def init():
         page_icon= '🤖'
     )
 
+def analyze_and_rewrite(api_key, user_input):
+    client = openai.OpenAI(api_key=api_key)
+    submit_messages = [
+        {"role": "system", "content": prompt1},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=submit_messages
+    )
+    suggestion_answer = response.choices[0].message.content
+    ans = json.loads(suggestion_answer)
+    answer_pandas = pd.DataFrame.from_dict(ans)
+    return answer_pandas
+
+
 def main():
     init()
-
     st.header('AI Text Analyzer and Rewriter 🤖')
-    st.markdown('Input the writing that you want to you want to check.')
+    st.markdown('Input the writing that you want to check.')
 
     # Set up key
     my_api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
-    
-    client = openai.OpenAI(api_key=my_api_key)
-    
-    user_input = st.text_area("Enter the text to analyze and rewrite:", "Your text here")
-    
 
-    if st.button('Submit'):
-        Submit_messages = [
-            {"role": "system", "content": prompt1},
-            {'role': 'user', 'content': user_input},
-        ]
-        response = client.chat.completions.create(
-            model = "gpt-3.5-turbo",
-            messages = Submit_messages
-        )
+    user_input = st.text_area("Enter the text to analyze and rewrite:", "Your text here")
+
+    if st.button('Submit') and my_api_key:
+        answer_pandas = analyze_and_rewrite(my_api_key, user_input)
 
         st.markdown('**AI response:**')
-        suggestion_answer = response.choices[0].message.content
-
-        ans = json.loads(suggestion_answer)
-
-        print (ans)
-        answer_pandas = pd.DataFrame.from_dict(ans)
-        print(answer_pandas)
         st.table(answer_pandas)
 
-
-main()
-# if my_api_key and user_input:
-#     tokenizer, model = initialize_model(my_api_key)
-#     analyzed_text = analyze_text(user_input)
-#     rewritten_text = rewrite_text(analyzed_text, tokenizer, model)
-    
-#     st.write("Rewritten text:")
-#     st.write(rewritten_text)
-#     st.divider()
-#     show_original = st.toggle('Show original text')
-#     if show_original:  
-#         st.write("Original text:")
-#         st.write(user_input)
-#     st.divider()
-#     on = st.toggle('Translater')
-
-#     if on:
-#         option = st.selectbox(
-#         "What language would you like to translate to?",
-#         ("German", "Spanish", "French"),
-        
-#         index=None,
-#         placeholder="Select Language...",
-#         )
-
-#         st.write('You selected:', option)
-#         st.write('Translation will be here')
+if __name__ == "__main__":
+    main()
