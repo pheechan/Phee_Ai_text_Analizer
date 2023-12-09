abf2754cdeb6d0eb7ed7d3429cc7ca9afb5df356
pheechan <6640208922@student.chula.ac.th>
2023-12-09 16:43:12 +0700

    20

--- main_Ai.py
+++ main_Ai.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 prompt1 = """Act as an AI writing analizer in English. You will receive a 
-            piece of generated writing from Ai and you will rewrite and improve the writing and make the writing more human-like making it less-detectable that it was from ai.
+            piece of writing from Ai and you will rewrite and improve the writing and make the writing more human-like making it less-detectable that it was from ai.
             Say only the writing that you generated, don't say anything else.
         """ 
 prompt2 = """Act as an AI writing analizer in German, French, Spanish. You will use the writing from the previous step and translate it to German.
@@ -36,13 +36,12 @@ def analyze_and_rewrite(api_key, user_input):
     print(f"suggestion_answer: {suggestion_answer}")
     st.markdown(suggestion_answer)
     st.markdown("DEBUG")
-    ans = json.loads(suggestion_answer)
+    # ans = json.loads(suggestion_answer)
 
-    # Convert to pandas DataFrame
-    answer_pandas = pd.DataFrame.from_records([ans])
+    # # Convert to pandas DataFrame
+    # answer_pandas = pd.DataFrame.from_records([ans])
+    answer_pandas = pd.DataFrame.from_dict([{"content": suggestion_answer}])
     return answer_pandas
-    # answer_pandas = pd.DataFrame.from_dict({"role": ["system", "user"], "content": [prompt1, user_input]})
-    # return answer_pandas
 
 def main():
     init()
