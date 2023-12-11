d7c788f086f7a557f28c97c32b871b5728d47999
pheechan <6640208922@student.chula.ac.th>
2023-12-11 23:02:31 +0700

    121

--- Prompts_text.py
+++ Prompts_text.py
@@ -50,7 +50,7 @@ prompt3 = """Act as an AI writing translater, and translate text to {}.
             "Part of Speech" : the part of speech of the vocabulary
             "Translation" : the translation of the vocabulary
             "Example" : an example sentence of the vocabulary in the language you just translate to.
-            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after "Translated text string" to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
+            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
             [
                 "Translated text string",
                 [
--- main_Ai.py
+++ main_Ai.py
@@ -58,7 +58,7 @@ def main():
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages_so_far,
-            #temperature = 0
+            #temperature = 
         )
         # Show the response from the AI in a box
         st.markdown('**AI response:**')
@@ -70,8 +70,8 @@ def main():
         st.markdown("--------------------------------")
 
         try:
-            if suggestion_answer[0] != '[' or suggestion_answer[len(suggestion_answer)-1] != ']' or suggestion_answer[len(suggestion_answer)-2] != ']':
-                st.markdown("Sorry, Please Submit again. 1")
+            if suggestion_answer[0] != '[' or suggestion_answer[len(suggestion_answer)-1] != ']':
+                st.markdown("Sorry, Please Submit again.")
             else : 
                 sd = json.loads(suggestion_answer)
             
@@ -86,9 +86,9 @@ def main():
                 print(suggestion_df)
                 st.table(suggestion_df)
         except json.JSONDecodeError:
-            st.markdown("Sorry, Please Submit again. 2")
+            st.markdown("Sorry, Please Submit again.")
         except IndexError:
-            st.markdown("Sorry, Please Submit again. 3")
+            st.markdown("Sorry, Please Submit again.")
 
 if __name__ == "__main__":
     main()
