7161339e710963c051c386cff56390015ff7412c
pheechan <6640208922@student.chula.ac.th>
2023-12-11 22:14:20 +0700

    113

--- main_Ai.py
+++ main_Ai.py
@@ -57,7 +57,7 @@ def main():
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages_so_far,
-            temperature = 0
+            temperature = 0.5
         )
         # Show the response from the AI in a box
         st.markdown('**AI response:**')
