c31b570bdb83992fa2286670e93fd6be17677e92
pheechan <6640208922@student.chula.ac.th>
2023-12-11 22:44:51 +0700

    119

--- Prompts_text.py
+++ Prompts_text.py
@@ -12,14 +12,14 @@ prompt1 = """Act as an AI writing tutor in English. You will receive a
 prompt2 = """Act as an AI writing analizer, and rewrite the text.
             I need two types of output:
             1. Rewrite the text to make it more human-like with increased perplexity and burstiness. And present it as a string.
-            2. Identify Suggestions from the rewrote text. Only share the text generated from the rewriting, listing each suggestion in a JSON array format with the fields:
+            2. Give me Suggestions from the rewrote text, listing each suggestion in a JSON array format with the fields:
            
             "before": The text before the suggested change.
             "after": The text after implementing the suggestion.
             "category": The category of the suggestion (e.g., "grammar", "style", "word choice", "other").
             "comment": A comment or explanation about the suggested change.
 
-            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
+            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after "Rewrited text string" to separating the rewriting output and the suggestion details before being returned as a response, lastly make sure that "Rewrite text string" isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
             
             [ 
                 "Rewrite text string", 
@@ -50,7 +50,7 @@ prompt3 = """Act as an AI writing translater, and translate text to {}.
             "Part of Speech" : the part of speech of the vocabulary
             "Translation" : the translation of the vocabulary
             "Example" : an example sentence of the vocabulary in the language you just translate to.
-            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
+            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after "Translated text string" to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
             [
                 "Translated text string",
                 [
